6f927cc27d807e2ded36dced62dea50c435c6e1b
Gustavo Fadel <Gustavo-959694@hotmail.com>
2022-10-07 00:27:45 -0300

    Feito os controller, routers, models e implementado a encriptação de senha usando o bcrypt

---
+++ controllers/userControllers.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const userController = {
+    register: async function (req, res) {
+        const selectedUser = await User.findOne({ email: req.body.email });
+        if (selectedUser) return res.status(400).send("Email already exists");
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password)
+        })
+        try {
+            const savedUser = await user.save();
+            res.send(savedUser);
+        } catch (error) {
+            res.status(400).send();
+        }
+    },
+    login: async function (req, res) {
+        const selectedUser = await User.findOne({ email: req.body.email });
+        if (!selectedUser) return res.status(400).send("Email or password incorrect");
+        
+        const passwordAndUserMatch = bcrypt.compareSync(req.body.password ,selectedUser.password);
+        if (!passwordAndUserMatch) return res.status(400).send("Email or password incorrect");
+
+        res.send("User Logged");
+    }
+};
+
+module.exports = userController;
